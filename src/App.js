3ba29973212783df12d45cd1ad4ddbe05ddad5c1
shubham3000 <shubhamsadhu02@gmail.com>
2022-05-23 11:43:05 +0530

    new

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {BrowserRouter as Router,Route,Routes} from "react-router-dom";
 import './Styles/global.css'
 
 import Home from './Pages/Home';
+import Nesplatform from './Pages/Product/Nesplatform';
 
 function App() {
   return (
@@ -12,12 +13,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home/>} />
-        {/* <Route path="/services" element={<Services/>}/>
-        <Route path="/about" element={<Aboutus/>} />
-        <Route path="/bloghighlights" element={<Bloghighlights/>} />
-        <Route path="/career" element={<Career/>}/>
-        <Route path="/ourwork" element={<Ourwork/>}/>
-        <Route path="/Blog" element={<Blog/>}/> */}
+        <Route path="/product" element={<Nesplatform/>}/>
       </Routes>
     </Router>
     
--- src/Partials/Header.js
+++ src/Partials/Header.js
@@ -2,46 +2,65 @@ import React from 'react'
 import { Link,NavLink, useParams } from 'react-router-dom'
 
 
-export default function header() {
+export default function header(props) {
+    const showNav = props.showNav??true;
+    const isWhite = props.isWhite??false;
+
   return (
     <>
-    <nav>
+    <nav style={{background: isWhite?'#10243E':"#FFFFFF"}}>
         <div className="container">
             <div className="navbar">
                 <div className="logo">
-                    <Link to={"/"}><img src='/images/Home/logo.png' height={20} width={150} /></Link>
+                    <Link to={"/"}><img src={isWhite?"/images/Home/logowhite1.png":"/images/Home/logo.png"} height={20} width={150} /></Link>
                 </div>
 
-                <div className="menu">
-                    <ul className="menu-list">
-                        <li>
-                            <NavLink to="/" >Products<i class="fal fa-angle-down"></i></NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/" >Use Cases<i class="fal fa-angle-down"></i></NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/" >Developers<i class="fal fa-angle-down"></i></NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/" >Resources<i class="fal fa-angle-down"></i></NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/" >Company<i class="fal fa-angle-down"></i></NavLink>
-                        </li>
-                    </ul>
-                </div>
+                {showNav?
+                    <div className="menu">
+                        <ul className="menu-list">
+                            <li>
+                                <NavLink to="/product" >Products<i class="fal fa-angle-down"></i>
+                                    <ul className='sub-menu'>
+                                        <li><a>Nes Platform</a></li>
+                                        <li><a>Nes Platform</a></li>
+                                        <li><a>Nes Platform</a></li>
+                                    </ul>
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/" >Use Cases<i class="fal fa-angle-down"></i>
+                                    <ul className='sub-menu'>
+                                        <li>Nes Platform</li>
+                                        <li>Nes Platform</li>
+                                        <li>Nes Platform</li>
+                                    </ul>
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/" >Developers<i class="fal fa-angle-down"></i></NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/" >Resources<i class="fal fa-angle-down"></i></NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/" >Company<i class="fal fa-angle-down"></i></NavLink>
+                            </li>
+                        </ul>
+                    </div>
+                :""}
 
-                <div className="search-nav">
-                    <ul>
-                        <li>
-                            <i class="far fa-search"></i>
-                        </li>
-                        <li>
-                            <button type='submit'>Get Started</button>
-                        </li>
-                    </ul>
-                </div>
+                {showNav?
+                    <div className="search-nav">
+                        <ul>
+                            <li>
+                                <a href='#'><i class="far fa-search"></i></a>
+                            </li>
+                            <li>
+                                <button type='submit'>Get Started</button>
+                            </li>
+                        </ul>
+                    </div>
+                :""}
             </div>
         </div>
     </nav>
