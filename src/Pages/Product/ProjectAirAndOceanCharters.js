0fd4cb6a423b47db6c014feb206a4d469e93cf41
shubham3000 <shubhamsadhu02@gmail.com>
2022-07-07 21:16:03 +0530

    product transportation pages updated

--- src/Pages/Product/ProjectAirAndOceanCharters.js
+++ src/Pages/Product/ProjectAirAndOceanCharters.js
@@ -8,30 +8,200 @@ export default function ProjectAirAndOceanCharters() {
     <>
     <Header showNav={true} isWhite={true} />
 
-<section style={{ background: "#10243E" }}>
+<section style={{ backgroundImage: "url(/images/product/air_charters/home.png)", backgroundSize: "cover", opacity: "5", height: "284px" }}>
     <div className="container">
-        <div className="top-section d-flex align-items-center mb-5">
-            <div className="row">
-                <div className="col-lg-7">
-                    <div className="product-topic">
-                        <h4>Project Air And Ocean Charters</h4>
-                    </div>
-                    <div className="product-heading mt-4">
-                        <h1>You Fill the Container. We Fill In the Rest.</h1>
-                    </div>
-                    <div className="product-paragraph mt-4">
-                        <p>With Flexport, all the ocean freight boxes are checked. Container location and contents, milestones and exceptions. It's all in the Flexport Platform for higher levels of reliability and available ocean space.
-                            </p>
-                    </div>
-                    <div className="product-get-started mt-4">
-                        <button type='submit'>Get Started</button>
-                    </div>
-                </div>
-
+        <div className="d-flex justify-content-center align-items-center" style={{height: "284px"}}>
+            <div className="product-heading">
+                <h1>Air Charters</h1>
             </div>
         </div>
     </div>
 </section>
+
+<section>
+      <div className="container mb-15">
+        <div className="product-item mt-15">
+          <div className="row">
+            <div className="col-lg-6 d-flex flex-column justify-content-center">
+              <div className="product-paragraph">
+                <p style={{ color: "#737171" }}>The NES service stands for superior quality on-demand air charter solutions, built on high dispatch reliability, diverse capabilities and customer support. A dedicated Coordinator for every flight ensures a seamless and smooth operation, end-to-end.</p>
+              </div>
+            </div>
+            <div className="col-lg-6">
+              <div className="product-img">
+                <img src='../images/product/air_charters/airimage1.png' />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="product-item mt-15">
+          <div className="row">
+            <div className="col-lg-6">
+              <div className="product-img">
+                <img src='../images/product/air_charters/heavy.png' />
+              </div>
+            </div>
+            <div className="col-lg-6 d-flex flex-column justify-content-center">
+              <div className="product-heading1">
+                <h3 style={{ color: "#403F3E" }}>Heavy Weight Champions</h3>
+              </div>
+              <div className="product-paragraph">
+                <p style={{ color: "#737171" }}>Aim to be a specialist in moving heavy cargo, from sea containers, large aero engines and helicopters, to generators and earth moving equipment, NES commits to doing the heavyweight lifting on your behalf to make it simple for you</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+</section>
+
+<section style={{background: "#10243E", padding: "96px 266px"}}>
+    <div className="continer">
+        <div className="nes-tailor">
+            <h3>
+            NES offers tailor made logistical solutions designed around the needs of VIPs, Large business conglomerates and Governments. NES expertise is proven at transporting time and security-sensitive cargo, with absolute discretion and confidentiality. NES can offer long-term total logistical solutions for VIP/Government/Military entities or cater to individual flights.
+            </h3>
+        </div>
+    </div>
+</section>
+
+<section>
+    <div className="container mt-15">
+        <div className="nes text-center">
+            <div className="nes-head">
+                <h3 style={{ color: "#403F3E" }}>NES Premium Charter Services</h3>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section>
+      <div className="container mb-15">
+        <div className="product-item mt-15">
+          <div className="row">
+            <div className="col-lg-6 d-flex flex-column justify-content-center">
+              <div className="product-paragraph">
+                <p style={{ color: "#737171" }}>NES Premium Charter Services is a suite of innovative, premium door-to-door services for clients to order as their needs dictate. This services provides customers the benefit and simplicity of a single point of contact and is a one-stop-shop solution for all your cargo matters, both in the air and on the ground. NES Air is the one name you can trust to get the most complex of jobs done.
+                    <br/><br/>
+                    Charter Select includes the following premium services:
+                    <br/><br/>
+                    <ul>
+                        <li>Load inspection</li>
+                        <li>Additional insurance</li>
+                        <li>Provision of pallets/ULDs</li>
+                        <li>Specialized loading equipment</li>
+                        <li>Break and build of pallets</li>
+                        <li>Ground transportation</li>
+                        <li>Documentation formalities</li>
+                        <li>Access to other charter aircraft including the A300-600, A330, and B747</li>
+                    </ul>
+                    </p>
+              </div>
+            </div>
+            <div className="col-lg-6">
+              <div className="product-img">
+                <img src='../images/product/air_charters/nesimage1.png' />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="product-item mt-15">
+          <div className="row">
+            <div className="col-lg-6">
+              <div className="product-img">
+                <img src='../images/product/air_charters/rapid.png' />
+              </div>
+            </div>
+            <div className="col-lg-6 d-flex flex-column justify-content-center">
+              <div className="product-heading1">
+                <h3 style={{ color: "#403F3E" }}>Rapid Response</h3>
+              </div>
+              <div className="product-paragraph">
+                <p style={{ color: "#737171" }}>Whether your aircraft is in need of a replacement engine, your factory is in need of a substitute part, you have perishables that need to be moved urgently, or your supply chain demands time critical deliveries, NES has the expertise, the team and the processes in place to launch a charter flight within the shortest possible time. NES can react with as little as 6 hours advance notice to offer the ultimate in reliable rapid response solutions for all your needs.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="product-item mt-15">
+          <div className="row">
+            <div className="col-lg-6 d-flex flex-column justify-content-center">
+                <div className="product-heading1">
+                    <h3 style={{ color: "#403F3E" }}>Humanitarian</h3>
+                </div>
+                <div className="product-paragraph">
+                    <p style={{ color: "#737171" }}>With our versatile capability to carry aid, equipment and even helicopters, NES provides valuable support to the humanitarian efforts and disaster relief of the world’s leading aid organizations. Our operations teams would asses the destination location and adequate arrangements would be made in advance to land on grass, and self-load and unload without the need for ground equipment to deliver critical humanitarian material.
+                        </p>
+                </div>
+                </div>
+                <div className="col-lg-6">
+                <div className="product-img">
+                    <img src='../images/product/air_charters/humanitrain.png' />
+                </div>
+                </div>
+            </div>
+        </div>
+
+        <div className="product-item mt-15">
+          <div className="row">
+            <div className="col-lg-6">
+              <div className="product-img">
+                <img src='../images/product/air_charters/shipment.png' />
+              </div>
+            </div>
+            <div className="col-lg-6 d-flex flex-column justify-content-center">
+              <div className="product-heading1">
+                <h3 style={{ color: "#403F3E" }}>Shipment Security</h3>
+              </div>
+              <div className="product-paragraph">
+                <p style={{ color: "#737171" }}>Critical relief shipments would need extraordinary care before it can be delivered in areas of military conflict. NES teams have the expertise to arrange for armed convoys to escort the shipments till delivery. Nes could also arrange storage of such shipments in TAPA certified facilities to ensure its safe prior to being distributed.
+                    </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="product-item mt-15">
+          <div className="row">
+            <div className="col-lg-6 d-flex flex-column justify-content-center">
+                <div className="product-heading1">
+                    <h3 style={{ color: "#403F3E" }}>Facilitating Collaboration</h3>
+                </div>
+                <div className="product-paragraph">
+                    <p style={{ color: "#737171" }}>Execution of a charter plan would need collaboration with multiple stakeholders at origin and destination, the NES platform facilitates this collaboration to ensure information is available readily and the customer has full control throughout the journey of the charter.</p>
+                </div>
+                </div>
+                <div className="col-lg-6">
+                <div className="product-img">
+                    <img src='../images/product/air_charters/facillating.png' />
+                </div>
+                </div>
+            </div>
+        </div>
+      </div>
+</section>
+
+<section style={{ background: "url('../images/product/air_charters/GetStartedBG.png')", backgroundSize: "cover" }}>
+        <div className="container mt-15">
+          <div className="ready d-flex flex-column justify-content-center">
+            <div className="product-topic1 mb-3">
+                <h4 style={{ color: "#FFFFFF" }}>Get Started</h4>
+            </div>
+            <div className="ready-heading mb-3">
+              <h3>Connect With us today</h3>
+            </div>
+            <div className="ready-heading-paragrapgh">
+              <p style={{ color: "#E6E5E5" }}>Click below to speak to our advisor</p>
+            </div>
+            <div className="reday-btn mt-3">
+              <button type='submit' style={{ background: "#FFFFFF", color: "#0C1E35" }}>Talk to advisor</button>
+            </div>
+          </div>
+        </div>
+</section>
+
+<Footer/>
     </>
   )
 }
